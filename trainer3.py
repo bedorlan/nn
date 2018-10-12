f45067b2a31303b2ee2e12625bff624ee3bcaae2
bedorlan <orlando.bedoya@openintl.com>
2018-10-12 16:53:32 -0500

    with dropout

--- trainer3.py
+++ trainer3.py
@@ -30,7 +30,7 @@ def getModel():
 def createModel():
     model = Sequential()
     model.add(LSTM(100, input_shape=(window_size-1, features), return_sequences=False))
-    # model.add(Dropout(0.2))
+    model.add(Dropout(0.2))
     # model.add(Flatten())
     # model.add(Dense(100, activation='relu'))
     model.add(Dense(1, activation='linear'))
