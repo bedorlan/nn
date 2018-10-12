28d7edfd57e80e8b336862dd9d537e810933eb42
bedorlan <orlando.bedoya@openintl.com>
2018-10-12 11:06:30 -0500

    refactoring & validation_split

--- trainer2.py
+++ trainer2.py
@@ -1,86 +1,12 @@
 import os
 import keras
-import numpy
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
-from sklearn.datasets import make_regression
-from sklearn.preprocessing import MinMaxScaler
-from numpy import array
+from data import getData
 
-# generate regression dataset
-raw_data = [
-    7847,
-    6528,
-    6441,
-    5372,
-    4200,
-    3431,
-    1868,
-    5676,
-    5442,
-    8044,
-    9938,
-    10067,
-    9655,
-    7790,
-    8155,
-    7782,
-    4576,
-    6465,
-    3092,
-    6375,
-    6308,
-    9266,
-    10320,
-    11297,
-    11053,
-    9199,
-    11256,
-    7258,
-    4484,
-    6493,
-    3784,
-    8938,
-    10207,
-    11594,
-    10957,
-    12183,
-    14452,
-    7790,
-    10163,
-    8162,
-    5493,
-    7141,
-    2128,
-    9041,
-    7880,
-    11275,
-    11732,
-    14142,
-    12633,
-    10411,
-    9245,
-    7975,
-    4134,
-    7046,
-    3537,
-    # 8549,
-    # 10477,
-    # 10629
-]
-# raw_data = [1, 4, 2, 5, 3]
-count = len(raw_data)
-
-X = array([i for i,x in enumerate(raw_data)])
-y = array(raw_data).reshape(-1, 1)
-scalarX, scalarY = MinMaxScaler(), MinMaxScaler()
-scalarX.fit(numpy.append(X, 100).reshape(-1, 1))
-scalarY.fit(y)
-X = scalarX.transform(X.reshape(-1, 1))
-y = scalarY.transform(y)
+X, y, scalarX, scalarY = getData()
 print X, y
-# define and fit the final model
 
 MODEL_FILE = 'models/out.model'
 
@@ -103,6 +29,6 @@ def createModel():
 
 model = getModel()
 while True:
-    history = model.fit(X, y, epochs=1000, verbose=0)
+    history = model.fit(X, y, epochs=1000, validation_split=0.05, verbose=0)
     model.save(MODEL_FILE)
     print 'loss=', history.history['loss'][-1]
