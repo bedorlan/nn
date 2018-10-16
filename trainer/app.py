de18be8967ad7382e3d45f91b23699375f83bfa9
bedorlan <orlando.bedoya@openintl.com>
2018-10-16 18:17:56 -0500

    listen to queues with pika

--- trainer/app.py
+++ trainer/app.py
@@ -1,4 +1,26 @@
+import os
 import pika
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 params = pika.ConnectionParameters(host='broker', connection_attempts=6, retry_delay=10)
 conn = pika.BlockingConnection(params)
+
+ch = conn.channel()
+ch.queue_declare(queue='get_models')
+
+for method, props, body in ch.consume('get_models'):
+    logging.info('new message=' + body)
+    dirs = [d for d in os.listdir('./models')]
+    response = str(dirs)
+
+    if props.reply_to:
+        logging.info('replying to=' + props.reply_to)
+        ch.basic_publish(
+            exchange='',
+            routing_key=props.reply_to,
+            properties=pika.BasicProperties(correlation_id=props.correlation_id),
+            body=response)
+
+    ch.basic_ack(delivery_tag=method.delivery_tag)
