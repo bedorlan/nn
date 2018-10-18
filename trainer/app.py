b42a50a61e0005f5678b58d781b022d75bbd66ca
bedorlan <orlando.bedoya@openintl.com>
2018-10-18 12:41:41 -0500

    python subscribing to ctrl

--- trainer/app.py
+++ trainer/app.py
@@ -1,26 +1,26 @@
 import os
-import pika
+import zmq
+import threading
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
-params = pika.ConnectionParameters(host='broker', connection_attempts=6, retry_delay=10)
-conn = pika.BlockingConnection(params)
+context = zmq.Context()
 
-ch = conn.channel()
-ch.queue_declare(queue='get_models')
 
-for method, props, body in ch.consume('get_models'):
-    logging.info('new message=' + body)
-    dirs = [d for d in os.listdir('./models')]
-    response = str(dirs)
+class Subscriber(threading.Thread):
+    def run(self):
+        logging.info('starting')
+        socket = context.socket(zmq.SUB)
+        socket.connect('tcp://ctrl:3002')
+        socket.subscribe('')
 
-    if props.reply_to:
-        logging.info('replying to=' + props.reply_to)
-        ch.basic_publish(
-            exchange='',
-            routing_key=props.reply_to,
-            properties=pika.BasicProperties(correlation_id=props.correlation_id),
-            body=response)
+        while True:
+            logging.info('waiting for message')
+            msg = socket.recv_string()
+            logging.info('new message=' + msg)
 
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+subscriber = Subscriber()
+subscriber.start()
+subscriber.join()
