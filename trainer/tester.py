be3cf33ebe2d074e4fbbb010840b43d757054447
bedorlan <orlando.bedoya@openintl.com>
2018-10-19 13:48:11 -0500

    refactoring

--- trainer/tester.py
+++ trainer/tester.py
@@ -8,21 +8,27 @@ import logging
 features = 2
 window_size = 6
 predictions_quantity = 6
+time_steps = window_size - 1
+
+
+def single_predict(model, y, scalarY, i):
+    ynew = y.copy()
+    X = ynew[i:][:time_steps]
+    X = X.reshape(-1, time_steps, features)
+
+    prediction = model.predict(X)
+    ynew[i + time_steps, 1] = prediction[0, 0]
+    return ynew
 
 
 def predict(model, y, scalarY):
     # predictions = [[0.0, 0.0] for _ in range(predictions_quantity)]
     # ynew = numpy.append(y.copy(), predictions).reshape(-1, features)
     ynew = y.copy()
-    time_steps = window_size - 1
     for i, _ in enumerate(ynew):
         if i + time_steps >= len(ynew):
             break
-        # ynew[i + time_steps, 0] = y[(i + time_steps) % 12, 0]
-        X = ynew[i:][:time_steps]
-        X = X.reshape(-1, time_steps, features)
-        prediction = model.predict(X)
-        ynew[i + time_steps, 1] = prediction[0, 0]
+        ynew = single_predict(model, ynew, scalarY, i)
 
     # logging.info('y')
     # logging.info(y)
